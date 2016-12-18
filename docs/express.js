20e30c820170154847b5e4292f85a505caefba29
Adrien Faure <Adrien Faure>
2016-12-18 22:50:58 +0100

    add draft.js

--- docs/express.js
+++ docs/express.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.use('/', express.static('docs'))
+app.use('/', express.static('./bin'))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
