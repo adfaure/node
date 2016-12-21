4ad94e113bbc2d3e2a39b7cc532c84c24a9bebc6
Adrien Faure <Adrien Faure>
2016-12-22 00:00:32 +0100

    begin integration of redux

--- src/app.js
+++ src/app.js
@@ -2,16 +2,34 @@ var index = require('file?name=[name].[ext]!../assets/index.html')
 var index = require('file?name=[name].[ext]!../assets/styles.css')
 var index = require('file?name=[name].[ext]!katex/dist/katex.min.css')
 
+import { EditorState } from 'draft-js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import katex from 'katex';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import { RichEditor } from './editor';
 
+let store = createStore(function reducer(state = { editorState: EditorState.createEmpty() }, action) {
+  switch (action.type) {
+    case 'INIT_EDITOR_STATE':
+      console.log("state", state);
+      console.log("action", action);
+      return Object.assign({}, state,  { editorState: action.editorState } )
+    case 'EDITOR_STATE_CHANGES':
+      console.log("state", state);
+      console.log("action", action);
+      return Object.assign({}, state,  { editorState: action.editorState } )
+
+    default:
+      return state;
+  }
+});
 
 ReactDOM.render(
-  <RichEditor />,
+  <Provider store={store}>
+    <RichEditor />
+  </Provider>,
   document.getElementById('app')
 );
-
-katex.render("c = \\pm\\sqrt{a^2 + b^2}", document.getElementById('katex'));
