2bdad3a8ee5ceabd1bbf14d5cd4062fb824b24c7
Adrien FAURE <adrien.faure2@gmail.com>
2017-01-23 22:58:58 +0100

    committtttted

--- src/notesEditor/editor.js
+++ src/notesEditor/editor.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Document } from './../document'
 
 import {Tabs, Tab} from 'material-ui/Tabs';
+import {List, ListItem} from 'material-ui/List';
+
+let files = [
+  'notes.md', 'README.md'
+]
 
 class Editor extends React.Component {
 
@@ -10,16 +15,31 @@ class Editor extends React.Component {
   }
 
   render() {
+    let leaves = files.map((elem, idx) => { return <ListItem primaryText={elem} key={idx} /> });
+    let tree = <List> {leaves} </List>;
+
+    let tabs = files.map((elem, idx) => {
+                return (<Tab key={idx}label={elem}>
+                          <Document gitConnection={this.props.git} username={this.props.credentials.username} filename={elem} repo={this.props.project} />
+                        </Tab>)
+                });
+
     return (
       <div>
-        <Tabs>
-          <Tab label="README.md" >
-            <Document gitConnection={this.props.git} username={this.props.credentials.username} filename="README.md" repo={this.props.project} />
-          </Tab>
-          <Tab label="notes.md" >
-            <Document gitConnection={this.props.git} username={this.props.credentials.username} filename="notes.md" repo={this.props.project} />
-          </Tab>
-        </Tabs>
+        <div className="row">
+            <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                <div className="box">
+                    {tree}
+                </div>
+            </div>
+            <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+                <div className="box">
+                  <Tabs>
+                      {tabs}
+                  </Tabs>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
