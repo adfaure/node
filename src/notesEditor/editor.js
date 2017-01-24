0a32930d2b31c166269276bc39ad350a4bf2f540
adfaure <adrien.faure2@gmail.com>
2017-01-24 11:48:21 +0100

    I'll explain when you're older!

--- src/notesEditor/editor.js
+++ src/notesEditor/editor.js
@@ -4,31 +4,80 @@ import { Document } from './../document'
 import {Tabs, Tab} from 'material-ui/Tabs';
 import {List, ListItem} from 'material-ui/List';
 
-let files = [
-  'notes.md', 'README.md'
-]
+import Divider from 'material-ui/Divider';
+import IconButton from 'material-ui/IconButton';
+import ActionNoteAdd from 'material-ui/svg-icons/action/note-add';
+import ActionClose from 'material-ui/svg-icons/navigation/close';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import Subheader from 'material-ui/Subheader';
+
 
 class Editor extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      currentTab :0
+      currentTab :0,
+
+      files: [
+        {
+          name: 'notes.md',
+          open: false
+        }, 
+        {
+          name: 'README.md',
+          open: true
+        }
+      ]
     }
+
+  }
+
+  closeFile(elem) {
+    elem.open = false;
+    this.setState({files: this.state.files})
+  }
+
+  openFile(elem) {
+    elem.open = true;
+    this.setState({files: this.state.files})
   }
 
   render() {
     let self = this;
-    let leaves = files.map((elem, idx) => { return <ListItem onClick={(e) => { self.setState({currentTab: idx}) }} primaryText={elem} key={idx} /> });
-    let tree = <List> {leaves} </List>;
+    let openFiles = this.state.files.filter(elem => elem.open).map((elem, idx) => { 
+      return <ListItem
+                rightIconButton={<IconButton onClick={() => {self.closeFile(elem)}}><ActionClose /></IconButton>}  
+                onClick={(e) => { self.setState({currentTab: idx}) }} 
+                primaryText={elem.name} key={idx} /> 
+    });
 
-    let tabs = files.map((elem, idx) => {
-                return (<Tab value={idx} key={idx} label={elem}>
-                          <Document gitConnection={this.props.git} username={this.props.credentials.username} filename={elem} repo={this.props.project} />
+    let availableFiles = this.state.files.filter(elem => !elem.open).map((elem, idx) => { 
+      return <ListItem 
+                onClick={(e) => { self.openFile(elem) } }
+                primaryText={elem.name} key={idx} />
+    });
+
+    availableFiles.push( <ListItem key={-1} primaryText="Add a note" rightIconButton={ <IconButton> <ActionNoteAdd /> </IconButton>}/> )
+
+    let tree = (<div>
+        <Subheader>Opened notes</Subheader>
+        <List> {openFiles} </List>
+        <Divider /> 
+        <Subheader>Available notes</Subheader>
+        <List> {availableFiles} </List>
+        </div>);
+
+    let tabs = this.state.files.filter(elem => elem.open).map((elem, idx) => {
+                return (<Tab value={idx} key={idx} label={elem.name}>
+                          <Document gitConnection={this.props.git} username={this.props.credentials.username} filename={elem.name} repo={this.props.project} />
                         </Tab>)
                 });
 
     return (
+
       <div>
         <div className="row">
             <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
@@ -45,6 +94,7 @@ class Editor extends React.Component {
             </div>
         </div>
       </div>
+
     );
   }
 
